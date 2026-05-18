80114f517701e23295d6c5895212d28252b337eb
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2026-05-18 18:44:41 +0530

    merged github changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Home from './pages/Home'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  )
 }
