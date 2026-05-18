bacdbe08b88b61f2556c50164a4a59979d89880c
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2026-05-18 18:48:32 +0530

    removed vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import Home from './pages/Home'
-import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
-  return (
-    <>
-      <Home />
-      <Analytics />
-    </>
-  )
+  return <Home />
 }
